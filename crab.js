911cae8a1f5617c68375c9b7c461ff513abb726a
zero wilson <zeuslfhj@hotmail.com>
2015-03-10 10:06:18 +0800

    add file type and fix bug

--- crab.js
+++ crab.js
@@ -7,7 +7,7 @@ var http = require('http')
 var argv = require('minimist')(process.argv.slice(2));
 var jsonPath = argv.har;
 var parentPath = argv.path || "";
-var resouceReg = /\.(jpg|gif|png|css|js|html|asp|jsp|php|ogg|mp3|xml)/i;
+var resouceReg = /\.(jpg|woff|jpeg|gif|png|css|js|html|asp|jsp|php|ogg|mp3|xml)/i;
 
 if (!jsonPath) {
   console.log("you must pass the har file path by parameter with key 'har'");
@@ -27,38 +27,48 @@ entries.forEach(function(entry, index, array){
   var url = entry.request ? entry.request.url : null;
 
   if (url && resouceReg.test(url)) {
-    var request = http.get( url, function(res){
-      var resData = '';
-        res.setEncoding('binary');
+    try{
+      var request = http.get( url, function(res){
+        var resData = '';
+          res.setEncoding('binary');
 
-        res.on('data', function(chunk){
-            resData += chunk
-        });
+          res.on('data', function(chunk){
+              resData += chunk
+          });
 
-        res.on("error", function( e ){
-          console.log("load resource failed:" + e.message);
-          console.error( e.stack );
-        });
+          res.on("error", function( e ){
+            console.log("load resource failed:" + e.message);
+            console.error( e.stack );
+          });
 
-        res.on('end', function(){
-          var lastInterrogation = url.lastIndexOf('?');
-          var src = lastInterrogation > -1 ? url.substring(0, lastInterrogation) : url;
-          var domain = src.substring(0, src.indexOf("/", src.indexOf("http://") + 8) + 1);
-          var fileName = src.substr( src.lastIndexOf('/') );
-          var dirPath = src.substring(0, src.lastIndexOf('/')).replace(domain, "");
-          dirPath = parentPath + dirPath;
+          res.on('end', function(){
+            var lastInterrogation = url.lastIndexOf('?');
+            var src = lastInterrogation > -1 ? url.substring(0, lastInterrogation) : url;
+            var domain = src.substring(0, src.indexOf("/", src.indexOf("http://") + 8) + 1);
+            var fileName = src.substr( src.lastIndexOf('/') );
+            var dirPath = src.substring(0, src.lastIndexOf('/')).replace(domain, "");
+            dirPath = parentPath + dirPath;
 
-          console.log( "url:" + url + "; dirpath:" + dirPath );
-          dir.mkdirSync(dirPath, 0777, true);
-          fs.writeFile(dirPath + fileName, resData, 'binary', function(err){
-              if (err){
-                console.error(dirPath + fileName + " has error");
-                throw err;
-              } 
-              console.log( dirPath + fileName + 'File saved.');
+            console.log( "url:" + url + "; dirpath:" + dirPath );
+            dir.mkdirSync(dirPath, 0777, true);
+            fs.writeFile(dirPath + fileName, resData, 'binary', function(err){
+                if (err){
+                  console.error(dirPath + fileName + " has error");
+                  throw err;
+                } 
+                console.log( dirPath + fileName + 'File saved.');
+            })
           })
-        })
-    });
+      });
+
+      request.setTimeout( 30000, function(){
+        console.log( arguments );
+        request.abort();
+        console.log("timeout");
+      });
+    }catch(e){
+      console.error( e.stack );
+    }
   };
 });
 
